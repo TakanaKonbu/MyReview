b240481d5bed34aaeb660faa06c1d069fb94ff6f
TakanaKonbu <takana.konbu150912@gmail.com>
2024-10-12 20:35:23 +0900

    リファクタリング

--- app/src/main/java/com/takanakonbu/myreview/category/CategoryListScreen.kt
+++ app/src/main/java/com/takanakonbu/myreview/category/CategoryListScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -85,8 +84,7 @@ fun CategoryItem(
         ) {
             Text(
                 text = categoryWithCount.category.name,
-                fontSize = 24.sp,
-                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.titleLarge,
                 modifier = Modifier.weight(1f)
             )
             Text(
