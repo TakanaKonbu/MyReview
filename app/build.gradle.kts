aa85bea5c7c4a97748f58dd48f1a19eee35bb63a
TakanaKonbu <takana.konbu150912@gmail.com>
2025-01-03 11:05:53 +0900

    バージョンコードの変更

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.takanakonbu.myreview"
         minSdk = 28
         targetSdk = 34
-        versionCode = 10
+        versionCode = 11
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
